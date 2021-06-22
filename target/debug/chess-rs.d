7c26ca8373ded2590aeb4b233d5462187135f837
meh <82113604+atmeh@users.noreply.github.com>
2021-06-22 13:40:49 -0300

    Delete target directory

--- target/debug/chess-rs.d
+++
@@ -1,1 +0,0 @@
-/Users/bello/Documents/chess-rs/target/debug/chess-rs: /Users/bello/Documents/chess-rs/src/draw.rs /Users/bello/Documents/chess-rs/src/main.rs /Users/bello/Documents/chess-rs/src/movement.rs
--- target/debug/deps/chess_rs.d
+++
@@ -1,7 +0,0 @@
-/Users/bello/Documents/chess-rs/target/debug/deps/chess_rs: src/main.rs src/movement.rs src/draw.rs
-
-/Users/bello/Documents/chess-rs/target/debug/deps/chess_rs.d: src/main.rs src/movement.rs src/draw.rs
-
-src/main.rs:
-src/movement.rs:
-src/draw.rs:
